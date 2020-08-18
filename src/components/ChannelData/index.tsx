1f6d1455d0a6d4f6f9611a022af9128313d2116e
Gabriel Fernandes <gabriel.costa@ccc.ufcg.edu.br>
2020-08-18 01:21:38 -0300

    Finish Channel Data component

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+
+import ChannelMessage, { Mention } from "../ChannelMessage";
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from "./styles";
 
 const ChannelData: React.FC = () => {
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+    useEffect(() => {
+        const div = messagesRef.current;
+
+        if (div) {
+            div.scrollTop = div.scrollHeight;
+        }
+    }, [messagesRef]);
+
     return (
         <Container>
-            <Messages />
+            <Messages ref={messagesRef}>
+                {Array.from(Array(15).keys()).map((n) => (
+                    <ChannelMessage
+                        author="Gabriel Shelby"
+                        date="18/08/2020"
+                        content="Bora jogar The Escapists"
+                    />
+                ))}
+
+                <ChannelMessage
+                    author="Monteiro Shelby"
+                    date="18/08/2020"
+                    content={
+                        <>
+                            <Mention>@Gabriel Shelby</Mention>, terminando de
+                            comer aqui, já vou
+                        </>
+                    }
+                    hasMention
+                    isBot
+                />
+            </Messages>
 
             <InputWrapper>
                 <Input type="text" placeholder="Conversar em #general-mourao" />
